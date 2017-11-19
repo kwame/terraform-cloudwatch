a741f52151fadf697d22c51da87d81d4c45aba59
kwame bahena <kwame@informatux.net>
2017-11-18 20:58:05 -0600

    arreglando typo en el terraform state file config

--- terraformstate.tf
+++ terraformstate.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "informatux-terraform-test"
-    key    = "terraform-testc/informatux-cloudwatch.tfstate"
+    key    = "terraform-test/informatux-cloudwatch.tfstate"
     region = "us-east-1"
     encrypt = true
   }
