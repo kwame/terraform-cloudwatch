ed4ba36a1fcade36583a2dbbdfc2b35c49eff4d8
Kwame Bahena <kwame@informatux.net>
2018-01-02 18:56:36 -0600

    adding .gitignore

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
-}
+#output "vpc_id" {
+#  value = "${module.vpc.vpc_id}"
+#}
 
 output "public_route_table_ids" {
   value = "${module.vpc.public_route_table_ids}"
